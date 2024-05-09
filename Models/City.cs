8e32e468858805b6a7832c2cb7fc96618714210a
Najoua Mehfoud <najoua.mehfoud@gmail.com>
2024-05-09 02:12:50 +0100

    KAN-33 City

--- Models/City.cs
+++ Models/City.cs
@@ -1,1 +1,38 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WeatherForecastApp.Models
+{
+    public class City
+    {
+        [Display(Name = "City Name :")]
+        public string Name { get; set; }
+
+        [Display(Name = "Temperature :")]
+        public float Temperature { get; set; }
+
+        [Display(Name = "Humidity :")]
+        public int Humidity { get; set; }
+
+        [Display(Name = "Pressure :")]
+        public int Pressure { get; set; }
+
+        [Display(Name = "Wind Speed :")]
+        public float Wind { get; set; }
+
+        [Display(Name = "Weather Condition :")]
+        public string Weather { get; set; }
+
+        [Display(Name = "Sunrise Time :")]
+        public string Sunrise { get; set; }
+
+        [Display(Name = "Sunset Time :")]
+        public string Sunset { get; set; }
+
+        [Display(Name = "Pays :")]
+        public string country { get; set; }
+
+       
+    }
+
+
+}

a0d580bf8ed9dee0a1daf1684e43b931a6bdac30
Najoua Mehfoud <najoua.mehfoud@gmail.com>
2024-05-09 02:15:59 +0100

    KAN-35 SearchByCity

---
+++ Models/SearchByCity.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WeatherForecastApp.Models
+{
+    public class SearchByCity
+    {
+        [Required(ErrorMessage ="City name is Empty!")]
+        [Display(Name = "City Name :")]
+        [StringLength(30,MinimumLength =2,ErrorMessage ="Invalid Input,Lenght Must be between 2 to 20 characters")]
+        public String CityName { get; set; }
+    }
+}
